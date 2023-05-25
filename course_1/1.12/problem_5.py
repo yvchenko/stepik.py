7e1bef56c5adfda43a332b6ea28e2f018ba2e3cb
yvchenko <nataliayuvchenko@gmail.com>
2023-05-25 18:20:44 +0300

    1.12: add problem 5

--- course_1/1.12/problem_5.py
+++ course_1/1.12/problem_5.py
@@ -6,27 +6,15 @@
 
 a, b, c = int(input()), int(input()), int(input())
 
-if a >= b and a >= c:
-    max_number = a
-elif b >= a and b >= c:
-    max_number = b
-else:
-    max_number = c
+if b >= a:
+    a, b = b, a
 
-if a <= b and a <= c:
-    min_number = a
-elif b <= a and b <= c:
-    min_number = b
-else:
-    min_number = c
+if c >= a:
+    a, c = c, a
 
-if max_number != a and min_number != a:
-    rest = a
-elif max_number != b and min_number != b:
-    rest = b
-elif max_number != c and min_number != c:
-    rest = c
+if c <= b:
+    b, c = c, b
 
-print(max_number)
-print(min_number)
-print(rest)
+print(a)
+print(b)
+print(c)
