0bd012fbe2669406e540d7ca51c5d46d0c1d5fb5
yvchenko <nataliayuvchenko@gmail.com>
2023-06-01 11:28:00 +0300

    2.06: add problem 4

--- course_1/2.06/problem_4.py
+++ course_1/2.06/problem_4.py
@@ -1,8 +1,8 @@
 # Напишите программу, на вход которой подаётся прямоугольная матрица в виде последовательности строк. После
 # последней строки матрицы идёт строка, содержащая только строку "end" (без кавычек, см. Sample Input).
 #
-# Программа должна вывести матрицу того же размера, у которой каждый элемент в позиции i, j равен сумме
-# элементов первой матрицы на позициях (i-1, j), (i+1, j), (i, j-1), (i, j+1). У крайних символов соседний элемент
+# Программа должна вывести матрицу того же размера, у которой каждый элемент в позиции i, y равен сумме
+# элементов первой матрицы на позициях (i-1, y), (i+1, y), (i, y-1), (i, y+1). У крайних символов соседний элемент
 # находится с противоположной стороны матрицы.
 #
 # В случае одной строки/столбца элемент сам себе является соседом по соответствующему направлению.
@@ -21,24 +21,24 @@ width = len(matrix[0])
 height = len(matrix)
 new_matrix = []
 
-for j in range(height):
+for y in range(height):
     row = []
-    for i in range(width):
-        left = matrix[j][i - 1]
-        if i + 1 == width:
-            right = matrix[j][0]
+    for x in range(width):
+        left = matrix[y][x - 1]
+        if x + 1 == width:
+            right = matrix[y][0]
         else:
-            right = matrix[j][i + 1]
-        up = matrix[j - 1][i]
-        if j + 1 == height:
-            down = matrix[0][i]
+            right = matrix[y][x + 1]
+        up = matrix[y - 1][x]
+        if y + 1 == height:
+            down = matrix[0][x]
         else:
-            down = matrix[j + 1][i]
+            down = matrix[y + 1][x]
         sum_all = left + right + up + down
         row.append(sum_all)
     new_matrix.append(row)
 
-for j in range(height):
-    for i in range(width):
-        print(new_matrix[j][i], end=' ')
+for y in range(height):
+    for x in range(width):
+        print(new_matrix[y][x], end=' ')
     print()
