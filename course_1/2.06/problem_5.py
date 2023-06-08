15fd6d195fd73e3a155fa2ef333f7ad2c60ec474
yvchenko <nataliayuvchenko@gmail.com>
2023-06-08 19:03:06 +0300

    2.06: solve problem 5

--- course_1/2.06/problem_5.py
+++ course_1/2.06/problem_5.py
@@ -8,43 +8,49 @@ matrix = [[0 for x in range(n)] for y in range(n)]
 width = len(matrix[0])
 height = len(matrix)
 
-add = 0
-
 x = 0
 y = 0
+
 number = 0
 
-while True:
-    number += 1
-    matrix[y][x] = number
-    if x == n - 1:
-        y += 1
-        break
-    x += 1
-while True:
-    number += 1
-    matrix[y][x] = number
-    if y == n - 1:
-        x -= 1
-        break
-    y += 1
-while True:
-    number += 1
-    matrix[y][x] = number
-    if x == 0:
-        y -= 1
-        break
-    x -= 1
-while True:
-    number += 1
-    matrix[y][x] = number
-    if y == 1:
-        x += 1
-        break
-    y -= 1
+lower_boundary = 0
+upper_boundary = n - 1
 
-
-
+while number < n ** 2:
+    if number < n ** 2:
+        while True:
+            number += 1
+            matrix[y][x] = number
+            if x == upper_boundary:
+                y += 1
+                break
+            x += 1
+    if number < n ** 2:
+        while True:
+            number += 1
+            matrix[y][x] = number
+            if y == upper_boundary:
+                x -= 1
+                break
+            y += 1
+    if number < n ** 2:
+        while True:
+            number += 1
+            matrix[y][x] = number
+            if x == lower_boundary:
+                y -= 1
+                break
+            x -= 1
+    lower_boundary += 1
+    upper_boundary -= 1
+    if number < n ** 2:
+        while True:
+            number += 1
+            matrix[y][x] = number
+            if y == lower_boundary:
+                x += 1
+                break
+            y -= 1
 
 for y in range(height):
     for x in range(width):
