0b9ec653d934513e668b1b6b993a59b811f976f2
yvchenko <nataliayuvchenko@gmail.com>
2023-06-09 21:14:22 +0300

    3.06: add problem 2

---
+++ course_1/3.06/problem_2.py
@@ -0,0 +1,30 @@
+# Имеется набор файлов, каждый из которых, кроме последнего, содержит имя следующего файла.
+# Первое слово в тексте последнего файла: "We".
+#
+# Скачайте предложенный файл. В нём содержится ссылка на первый файл из этого набора.
+#
+# Все файлы располагаются в каталоге по адресу:
+# https://stepic.org/media/attachments/course67/3.6.3/
+#
+# Загрузите содержимое последнего файла из набора, как ответ на это задание.
+
+
+import requests
+
+with open('problem_2_dataset.txt') as source:
+    address = source.readline().strip()
+
+file = requests.get(address)
+domain = 'https://stepic.org/media/attachments/course67/3.6.3/'
+file_content = file.text
+
+# while "We" not in file_content:
+#     new_address = domain + file_content
+#     file_content = requests.get(new_address).text
+#     print(file_content)
+
+with open('problem_2_done.txt', 'w') as done:
+    while "We" not in file_content:
+        new_address = domain + file_content
+        file_content = requests.get(new_address).text
+    done.write(file_content + '\n')
