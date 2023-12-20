9b77c5b8a5dd032dd5cdf2ce889814241d0400ff
yvchenko <nataliayuvchenko@gmail.com>
2023-12-20 15:18:00 +0400

    course 2, step 3.02: solve problem 4

---
+++ course_2/3.02/problem_4.py
@@ -0,0 +1,10 @@
+# Вам дана последовательность строк.
+# Выведите строки, содержащие обратный слеш "\".
+
+import sys
+import re
+
+for line in sys.stdin:
+    line = line.rstrip()
+    if re.search(r"\\", line):
+        print(line)
